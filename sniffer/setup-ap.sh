90861aff02751d49c7d0e0c13688f1882560a521
vitorsoprani <vitor.spassamani@gmail.com>
2026-05-12 09:57:25 -0300

    feature: script que cria um ap wifi

---
+++ sniffer/setup-ap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+INTERFACE=wlxe894f6281fb9
+CON_NAME=Hotspot
+AP_SSID=test-ap
+AP_PSK=12345678
+
+# WIFI SETUP
+echo "Setting up network..."
+MAIN_IF=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
+echo "Main network interface with internet detected: $MAIN_IF"
+
+sudo sysctl -w net.ipv4.ip_forward=1
+
+sudo nmcli con delete "$CON_NAME" 2>/dev/null
+sudo nmcli con add type wifi ifname "$INTERFACE" con-name "$CON_NAME" autoconnect no ssid "$AP_SSID"
+sudo nmcli con modify "$CON_NAME" \
+    802-11-wireless.mode ap \
+    802-11-wireless.band bg \
+    802-11-wireless.channel 6 \
+    802-11-wireless-security.key-mgmt wpa-psk \
+    802-11-wireless-security.proto rsn \
+    802-11-wireless-security.pairwise ccmp \
+    802-11-wireless-security.group ccmp \
+    802-11-wireless-security.psk "$AP_PSK" \
+    ipv4.method shared
+sudo nmcli con up "$CON_NAME"
+
+# Limpa regras antigas de encaminhamento para evitar conflitos no testbed
+sudo iptables -t nat -F POSTROUTING
+sudo iptables -F FORWARD
+
+# Habilita o NAT (Masquerade) na interface que tem internet
+sudo iptables -t nat -A POSTROUTING -o "$MAIN_IF" -j MASQUERADE
+
+# Permite o tráfego saindo da interface do Hotspot para a Internet
+sudo iptables -A FORWARD -i "$INTERFACE" -o "$MAIN_IF" -j ACCEPT
+
+# Permite o tráfego de volta (da Internet para o Hotspot) para conexões já estabelecidas
+sudo iptables -A FORWARD -i "$MAIN_IF" -o "$INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT
